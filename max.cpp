099a9a8a7812820c799c42b0973e54460b7dcf88
Akashneel Saha <asaha-mirp@mit.edu>
2017-05-31 06:53:12 +0530

    Adding cpp files

---
+++ max.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i=1, n;
+cout << "Enter any number : " << endl;
+cin >> n ;
+for (i=1; i<=n; i+=2)
+{
+cout << "the odd number is : " << i <<endl;
+}
+return 0;
+}
